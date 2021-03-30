ba9b94ca75732ca6f3bb03bb5295912d6706a185
Xinru Zou <xz5177@rit.edu>
2021-03-30 17:29:59 -0400

    mark recipe

---
+++ populate_quantity.py
@@ -0,0 +1,21 @@
+import psycopg2
+import ast
+from datetime import datetime
+import random
+
+
+conn = psycopg2.connect(
+    host="reddwarf.cs.rit.edu",
+    database="p320_02a",
+    user="p320_02a",
+    password="mdzpxSyGJSvn",
+)
+
+cur = conn.cursor()
+
+
+num = random.randint(1,50)
+
+cur.execute("SELECT * from public.ingredient_to_recipe WHERE ingredient_quantity = %s ", ())
+
+
