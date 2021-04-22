41610a93c9142c7925db168709db316f6d8ff930
xz5177 <60419147+xz5177@users.noreply.github.com>
2021-04-22 01:24:25 -0400

    update average rating in recipe table

--- most_recent_recipt_made.py
+++ most_recent_recipt_made.py
@@ -37,4 +37,28 @@ def most_recent_recipe():
         print(" %-2s %-45s %-15s" % (str(num) + ".", name, i))
         num += 1
 
+        
+def update_rating():
+    """
+    Update the average rating in the recipe table for the newly populated data into the rating table
+    :return:
+    """
+    cur.execute("SELECT DISTINCT recipe_id FROM public.rating")
+    result = cur.fetchall()
 
+    for i in result:
+        i = str(i)
+        i = i[1:-2]
+        cur.execute("SELECT rating from public.rating WHERE recipe_id = %s", (i,))
+        r = cur.fetchall()
+        sum1 = 0
+        count = 0
+        for a in r:
+            a = str(a)
+            a = a[10:11]
+            a = int(a)
+            sum1 += a
+            count += 1
+        rating = int(round(sum1 / count, 0))
+        cur.execute("UPDATE public.recipe SET rating = %s WHERE recipe_id = %s", (rating, i))
+        conn.commit()
